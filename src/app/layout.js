c579ee0303094151b1818b0679d458f907cd1cf4
Paul Fomitchev <paul.fomitchev@gmail.com>
2026-03-17 13:44:19 -0400

    image prefetch

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,33 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ThemeProvider from "./components/ThemeProvider";
 import SplashCursor from "./components/SplashCursor";
+import FlavorFlickScreenshot from "../../assets/flavorflick-screenshot.png";
+import FeelifyScreenshot from "../../assets/feelify-screenshot.png";
+import HandInHandScreenshot from "../../assets/handinhand-screenshot.png";
+import SudokuScreenshot from "../../assets/sudokusolver-screenshot.png";
+import KinKitchenScreenshot from "../../assets/kinkitchen-screenshot.png";
+
 export const metadata = {
   title: "Paul Fomitchev",
   description: "Paul Fomitchev's personal portfolio",
 };
 
+const projectImages = [
+  FlavorFlickScreenshot.src,
+  FeelifyScreenshot.src,
+  HandInHandScreenshot.src,
+  SudokuScreenshot.src,
+  KinKitchenScreenshot.src,
+];
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {projectImages.map((src) => (
+          <link key={src} rel="prefetch" href={src} as="image" />
+        ))}
+      </head>
       <body className={GeistMono.className}>
         <ThemeProvider>
           <SplashCursor />
