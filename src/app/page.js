da3516188733889a838083ce5b4271b1e7e88031
Paul Fomitchev <paul.fomitchev@gmail.com>
2026-05-14 01:32:54 -0400

    feat: clear page content on NPM branch to show only vanta bg

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,3 @@
 export default function Page() {
-  return (
-    <main style={{ 
-      display: 'flex', 
-      alignItems: 'center', 
-      justifyContent: 'center', 
-      minHeight: '100vh' 
-    }}>
-    </main>
-  );
+  return null;
 }
